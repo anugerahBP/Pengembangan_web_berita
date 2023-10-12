7aa591673564ec06dd163eb48ab9601703013bad
anugerahBP <anugerahbpratama89@gmail.com>
2023-10-12 15:23:30 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$server   = "localhost";
+$username = "root";
+$password = "";
+$database = "berita";
+
+$koneksi = mysqli_connect($server, $username, $password) or die('Koneksi gagal');
+
+if ($koneksi) {
+	mysqli_select_db($koneksi,$database) or die('Database belum dibuat');
+}
